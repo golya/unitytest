8c2b7704b2f69f639826dcc4d043d9046d84c129
Ádám Gólya <adam.stork@gmail.com>
2015-04-12 21:36:02 +0200

    scipts just for practice

---
+++ scripts/AccelScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AccelScript : MonoBehaviour {
+	void Update () {
+		transform.Translate (Input.acceleration.x * Time.deltaTime * 10, 0,
+		                     Input.acceleration.z * Time.deltaTime);
+	}
+}
